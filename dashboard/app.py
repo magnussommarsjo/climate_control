cd78492cc05340b274fbc0df9892adf8fac4dc33
Magnus Sommarsjö <magnus.lengquist@gmail.com>
2022-09-12 08:59:57 +0200

    Add Update button

--- dashboard/app.py
+++ dashboard/app.py
@@ -4,25 +4,32 @@ import plotly.express as px
 from data import load_data, Schema
 
 
-app = Dash(__name__)
-
-
 class ID(str, enum.Enum):
     MAIN_GRAPH = "main_graph"
+    UPDATE_BUTTON = "update_button"
 
 
-data = load_data("")
+app = Dash(__name__)
+
+app.layout = html.Div(
+    [
+        html.H1("Home Climate dashboard"),
+        html.Button("Update", id=ID.UPDATE_BUTTON),
+        dcc.Graph(id=ID.MAIN_GRAPH),
+    ]
+)
 
 
-def create_main_graph() -> dcc.Graph:
+@app.callback(
+    Output(ID.MAIN_GRAPH, "figure"),
+    Input(ID.UPDATE_BUTTON, "n_clicks"),
+)
+def update_figure(_):
+    data = load_data("")
     fig = px.line(data, x=Schema.TIME, y=Schema.VALUE, color=Schema.CATEGORY)
     fig.update_layout(title="Sensor values", xaxis_title="Time", yaxis_title="Value")
 
-    graph = dcc.Graph(id=ID.MAIN_GRAPH, figure=fig)
-    return graph
-
-
-app.layout = html.Div([html.H1("Home Climate dashboard"), create_main_graph()])
+    return fig
 
 
 if __name__ == "__main__":
