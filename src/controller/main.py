60be8ac8b45d01d16cf19d7bfb47f2ac1f282de6
Magnus Sommarsjö <magnus.lengquist@gmail.com>
2022-11-14 14:04:53 +0100

    Extracted functions out of main()

--- src/controller/main.py
+++ src/controller/main.py
@@ -14,6 +14,7 @@ import traceback
 import threading
 import time
 from datetime import datetime
+from typing import List
 from flask import Response, make_response
 
 
@@ -43,42 +44,90 @@ from dashboard import app
 from husdata.controllers import Rego1000
 from controller.strategies import StrategyHandler, OffsetOutdoorTemperatureStrategy
 from controller.sensors import continuous_logging, Sensor
-from controller.storage import InfluxStorage, CsvStorage
+from controller.storage import InfluxStorage, CsvStorage, Storage
 
 SAMPLE_TIME = 60
 H60_IP_ADDRESS = "192.168.1.12"
 HOST = os.getenv("HOST", "0.0.0.0")
 PORT = os.getenv("PORT", 80)
 INFLUXDB_TOKEN = os.getenv("INFLUXDB_TOKEN")
+INFLUXDB_PORT = os.getenv("INFLUXDB_PORT", 8086)
+INFLUXDB_ADDRESS = os.getenv("INFLUXDB_ADDRESS", "influxdb2")
+
 
 def main():
     log.info("Main entrypoint started")
 
-    # If we have a token for InfluxDB the assume that is the storage to use. 
-    # Otherwise we fallback on a simple CsvStorage.
+    storage = set_up_storage()
+
+    first_floor_sensor = Sensor(name="first_floor", address="192.168.1.21")
+    rego = Rego1000(H60_IP_ADDRESS)
+
+    strategy_threads = set_up_strategies(
+        rego=rego, indoor_temp_sensor=first_floor_sensor
+    )
+    logging_thread = set_up_logging(
+        rego=rego, indoor_temp_sensor=first_floor_sensor, storage=storage
+    )
+
+    # Start all threads. These are 'daemon threads and will be killed as soon as
+    # main thread ends. In this case it will be when the dashboard server is closed.
+    threads = [logging_thread, *strategy_threads]
+    _ = [thread.start() for thread in threads]
+
+    # Health check.
+    # Needs to be defined heer instead of in the app.py module due to checking status
+    # of threads.
+    @app.app.server.route("/health")
+    def health_check():
+
+        # Check threads
+        if any(not thread.is_alive() for thread in threads):
+            return Response("{status: not_ok}", status=500, mimetype="application/json")
+
+        return Response("{status: ok}", status=200, mimetype="application/json")
+
+    # Start the dashboard application server
+    # NOTE: Debug mode set to 'True' messes upp logging to csv files somehow.
+    # Related to threads?
+    app.app.run(host=HOST, port=PORT, debug=False)
+
+
+def set_up_storage() -> Storage:
+    """Set up storage.
+
+    If we have a token for InfluxDB the assume that is the storage to use.
+    Otherwise we fallback on a simple CsvStorage.
+    """
+
     if INFLUXDB_TOKEN:
         storage = InfluxStorage(
-            address="influxdb2",
-            port=8086,
+            address=INFLUXDB_ADDRESS,
+            port=INFLUXDB_PORT,
             token=INFLUXDB_TOKEN,
             org="climate-control",
             bucket="climate-control",
         )
     else:
         storage = CsvStorage()
-        
+
     log.info(f"Storage {storage} instantiated.")
+    return storage
 
 
-    first_floor_sensor = Sensor(name="first_floor", address="192.168.1.21")
-    rego = Rego1000(H60_IP_ADDRESS)
+def set_up_logging(
+    indoor_temp_sensor: Sensor, rego: Rego1000, storage: Storage
+) -> threading.Thread:
+    """Set up logging.
 
-    # Setting up continuous logging signals both from custom sensors as well as
-    # the Rego1000 controller via H60Gateway
+    Setting up continuous logging signals both from custom sensors as well as
+    the Rego1000 controller via H60Gateway
+    """
+
     def get_data_from_sensors() -> dict:
         """Function to be used in continuous logging"""
-        if first_floor_sensor.update():
-            sensor_data = first_floor_sensor.to_dict()
+        if indoor_temp_sensor.update():
+            sensor_data = indoor_temp_sensor.to_dict()
         else:
             sensor_data = {}
 
@@ -97,42 +146,26 @@ def main():
         daemon=True,
     )
 
-    # Setting up Control strategies
+    return logging_thread
+
+
+def set_up_strategies(
+    rego: Rego1000, indoor_temp_sensor: Sensor
+) -> List[threading.Thread]:
+    """Setting up Control strategies"""
     offset_strategy = OffsetOutdoorTemperatureStrategy(
         rego=rego,
-        indoor_temperature_callable=lambda: first_floor_sensor.temperature,
+        indoor_temperature_callable=lambda: indoor_temp_sensor.temperature,
         influence=2,
     )
     strategy_handler = StrategyHandler()
     strategy_handler.register_strategy(offset_strategy)
 
     strategy_thread = threading.Thread(
-        name="offset_strategy",
-        target=strategy_handler.run_strategies,
-        daemon=True
+        name="offset_strategy", target=strategy_handler.run_strategies, daemon=True
     )
 
-    # Start all threads. These are 'daemon threads and will be killed as soon as
-    # main thread ends. In this case it will be when the dashboard server is closed.
-    threads = [logging_thread, strategy_thread]
-    _ = [thread.start() for thread in threads]
-
-    # Health check. 
-    # Needs to be defined heer instead of in the app.py module due to checking status 
-    # of threads. 
-    @app.app.server.route("/health")
-    def health_check():
-
-        # Check threads
-        if any(not thread.is_alive() for thread in threads):
-            return Response("{status: not_ok}", status=500, mimetype='application/json')
-
-        return Response("{status: ok}", status=200, mimetype='application/json')
-
-    # Start the dashboard application server
-    app.app.run(host=HOST, port=PORT, debug=False)
-    # NOTE: Debug mode set to 'True' messes upp logging to csv files somehow.
-    # Related to threads?
+    return [strategy_thread]
 
 
 if __name__ == "__main__":
