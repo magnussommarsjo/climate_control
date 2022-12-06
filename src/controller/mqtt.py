324e57df6a001773a0793234f348b219df2a34e7
Magnus Sommarsjö <magnus.lengquist@gmail.com>
2022-12-06 15:51:41 +0100

    Replace MQTT client with custom MQTT Handler

--- src/controller/mqtt.py
+++ src/controller/mqtt.py
@@ -5,8 +5,10 @@ Subscribe to temperature sensors and H60?
 
 """
 
+from collections import defaultdict
 import logging
 from queue import Queue
+from typing import Callable
 
 import paho.mqtt.client as mqtt
 
@@ -15,29 +17,76 @@ from controller.config import read_config
 log = logging.getLogger(__name__)
 config = read_config()
 
-message_que: Queue[mqtt.MQTTMessage]  = Queue()
-client = mqtt.Client(client_id=config.MQTT_CLIENT_ID)
+class MQTTHandler:
+    def __init__(self, client_id: str, mqtt_host: str, mqtt_port: int) -> None:
+        self.client = mqtt.Client(client_id=client_id)
+        self.mqtt_host = mqtt_host
+        self.mqtt_port = mqtt_port
+        self.message_que: Queue[mqtt.MQTTMessage] = Queue()
+        self._callback_registry: dict[str, list[Callable]] = defaultdict(list)
 
-@client.connect_callback()
-def _on_connect(client: mqtt.Client, userdata, flags, rc: mqtt.ReasonCodes) -> None:
-    if rc == 0:
-        log.info(f"Connected to MQTT Broker {config.MQTT_HOST} on port {config.MQTT_PORT}")
-    else:
-        log.info(f"Failed_to_connect, return code {rc}")
+        self._set_up_callback()
 
-@client.message_callback()
-def _on_message(client: mqtt.Client, userdata, message: mqtt.MQTTMessage) -> None:
-    log.info("\nmessage received " + str(message.payload.decode("utf-8")))
-    log.info(f"{message.topic=}")
-    log.info(f"{message.qos=}")
-    log.info(f"{message.retain=}")
+    def _set_up_callback(self) -> None:
+        """Set up basic callbakc for logging and message que"""
 
-    message_que.put(message)
+        @self.client.connect_callback()
+        def _on_connect(client: mqtt.Client, userdata, flags, rc: mqtt.ReasonCodes) -> None:
+            if rc == 0:
+                log.info(f"Connected to MQTT Broker {config.MQTT_HOST} on port {config.MQTT_PORT}")
+            else:
+                log.info(f"Failed_to_connect, return code {rc}")
+                
+        @self.client.message_callback()
+        def _on_message(client: mqtt.Client, userdata, message: mqtt.MQTTMessage) -> None:
+            log.info("\nmessage received " + str(message.payload.decode("utf-8")))
+            log.info(f"{message.topic=}")
+            log.info(f"{message.qos=}")
+            log.info(f"{message.retain=}")
+            self._execute_callbacks_from(topic, message)
 
 
+    def _execute_callbacks_from(self, topic: str, message: str):
+        subscriptions = [sub for sub in self._callback_registry.keys() if mqtt.topic_matches_sub(sub, topic)]
+        for sub in subscriptions:
+            for callback in self._callback_registry[sub]:
+                callback(topic, message)
+    
+    def connect(self):
+        self.client.connect(host=self.mqtt_host, port=self.mqtt_port)
 
-def connect_client_to_broker(client: mqtt.Client) -> None:
-    client.connect(host=config.MQTT_HOST, port=config.MQTT_PORT)
+    def disconnect(self):
+        self.client.disconnect()
+
+    def start(self):
+        self.client.loop_start()
+    
+    def stop(self):
+        self.client.loop_stop()
+
+    def register_callback(self, topic: str, callback: Callable[[str, str], None]):
+        """Register callback to MQTT topic.
+        
+        Callback must have two arguments, `topic` and `message` and return `None`.
+        >>> def my_callback(some_topic: str, some_message: str) -> None:
+        >>>     ...
+        >>>
+        >>> mqtt_handler.register_callback("some/topic", my_callback)
+        """
+        self.client.subscribe(topic)
+        self._callback_registry[topic].append(callback)
+        log.info(f"Callback {callback.__name__} registered to topic {topic}")
+
+    def unregister_callback(self, topic: str, callback: Callable):
+        """Unregister the callback function earlier registered"""
+        if callback in self._callback_registry[topic]:
+            self._callback_registry[topic].remove(callback)
+            log.info(f"Callback {callback.__name__} unregistered from topic {topic}")
+
+        if not self._callback_registry[topic]:
+            # no one are registered to the topic, unsubscribe
+            self.client.unsubscribe(topic)
+
 
 
 if __name__ == "__main__":
@@ -47,17 +96,27 @@ if __name__ == "__main__":
     log.setLevel(logging.INFO)
     log.addHandler(logging.StreamHandler(sys.stdout))
 
-    connect_client_to_broker(client)
     topic = "+/sensor/reading"
-    client.subscribe(topic=topic)
 
+    def my_callback(*args):
+        print("Callback executed")
 
-    client.loop_start()
+    def my_other_callback(*args):
+        print("This was executed as well")
 
-    for _ in range(10): # limit to 10 messages
-        message = message_que.get(timeout=60)
-        print("Got message")
+    handler = MQTTHandler("test_handler", config.MQTT_HOST, config.MQTT_PORT)
 
-    client.loop_stop()
-    client.disconnect()
+    handler.connect()
+    handler.register_callback(topic, my_callback)
+    handler.register_callback(topic, my_other_callback)
+    handler.start()
+
+    time.sleep(10)
+
+    handler.unregister_callback(topic, my_other_callback)
+    time.sleep(10)
+
+    handler.stop()
+    handler.disconnect()
+
     print("Main finished")
